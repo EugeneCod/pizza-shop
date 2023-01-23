83f20f06a1abbf45a9085468c17a51b89fabf6f7
Eugene Korobkin <eugene_korobkin@mail.ru>
2023-01-23 22:13:40 +0300

    feat: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import Loadable from 'react-loadable';
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { FullPizza } from './components';
 import MainLayout from './layouts/MainLayout';
-import { Home, Cart, NotFound } from './pages';
+import Home from './pages/Home';
+// При lazy loading страницы при помощи встроенной функции React.lazy нужный компоент оборачивается в <Suspense />
+// В <Suspense /> передается fallback(разметка, размещаемая до окончания загрузки страницы)
+// const Cart = lazy(() => import(/* webpackChunkName: 'CartPage' */'./pages/Cart'));
+const NotFound = lazy(() => import(/* webpackChunkName: 'NotFoundPage' */ './pages/NotFound'));
+const FullPizza = lazy(
+  () => import(/* webpackChunkName: 'FullPizzaPage' */ './pages/FullPizza/FullPizza'),
+);
+
+// Библиотека react-loadable применяется при необходимости серверного рендеринга,
+// т.к. React.lazy не имеет функционала для этого
+const Cart = Loadable({
+  loader: () => import(/* webpackChunkName: 'CartPage' */ './pages/Cart'),
+  loading: () => <div>Загрузка корзины...</div>,
+});
 
 function App() {
   return (
@@ -11,8 +26,22 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           <Route path="" element={<Home />} />
           <Route path="cart" element={<Cart />} />
-          <Route path="pizza/:id" element={<FullPizza />} />
-          <Route path="*" element={<NotFound />} />
+          <Route
+            path="pizza/:id"
+            element={
+              <Suspense fallback={<div>Загрузка товара...</div>}>
+                <FullPizza />
+              </Suspense>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <Suspense fallback={<div>Загрузка страницы...</div>}>
+                <NotFound />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </div>
