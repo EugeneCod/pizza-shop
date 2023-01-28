2a228cf2a30869b7aa99e7a4ca3c34fa7221ace5
Eugene Korobkin <eugene_korobkin@mail.ru>
2023-01-28 16:02:16 +0300

    Updates

---
+++ static/js/286.bab34571.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_pizza=self.webpackChunkreact_pizza||[]).push([[286],{286:function(n,e,c){function t(n,e){return console.log(111),n+e}c.r(e),c.d(e,{add:function(){return t}})}}]);
+//# sourceMappingURL=286.bab34571.chunk.js.map
